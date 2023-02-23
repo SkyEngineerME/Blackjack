df74064fafcf126b6762951a57acff87e5a2bad2
wissawa_cosplay <110962879+TheeraphanPhanwattanasin@users.noreply.github.com>
2023-02-23 17:13:46 +0700

    edit in some wrong cases

--- BetP().cpp
+++ BetP().cpp
@@ -1,47 +1,35 @@
 #include <iostream>
 #include <windows.h>
+#include <string>
+#include <cstdlib>
 using namespace std;
 
 /* color ref. https://www.youtube.com/watch?v=MvX4tVETjHk&t=19s&ab_channel=CodeLeader */
-/* this function doesn't work in some cases (bug when put alphabat in it, cannot put 2 data in one time)*/
+/* can solve when put invalid input (like alphabet a,b,cat,y2000k,a4.5b or more two data like 100 200, 1.5 2.5 3.5,...)*/
+/* cannot solve in only this case (ex. 123abc, 100cat, ...)*/
 
 void BetP(int &moneyp){
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h,14);
     cout << "----888-------------------------888----\n";
     cout << "$$$ Put Your Money To Bet Your Fate $$$\n";
-    cout << "----888-------------------------888----\n";
+    cout << "----888-------------------------888----\n\n";
 
     while(true){
+    SetConsoleTextAttribute(h,14);
     cout << "Put your money (You have left ";
     SetConsoleTextAttribute(h,5);
     cout << moneyp << "$";
     SetConsoleTextAttribute(h,14);
     cout << ") : ";
 
- double betp;
-    cin >> betp;
-    if(betp <= 0) {
-        SetConsoleTextAttribute(h,4);
-        cout << "-----------------------------------------------------------------------\n";
-        cout << "$$$ Invalid input Man! Give me your money! Try again and quickly!!! $$$\n";
-        cout << "-----------------------------------------------------------------------\n";
-    }
-    else if(betp > moneyp){
-        SetConsoleTextAttribute(h,4);
-        cout << "--------------------------------------------------------------------------------\n";
-        cout << "$$$ Are you serious? You put money more than you have! Try again the poor!!! $$$\n";
-        cout << "--------------------------------------------------------------------------------\n";
-    }
-    else if(betp - (int) betp != 0) {
-        SetConsoleTextAttribute(h,4);
-        cout << "---------------------------------------------------------------------------------\n";
-        cout << "$$$ I don't want some kind of pocket change...Try again with integer numbers. $$$\n";
-        cout << "---------------------------------------------------------------------------------\n";
-    }
-    else{
+    string text;
+    getline(cin,text);
+    double betp = atof(text.c_str());
+    if(betp > 0 && betp <= moneyp && betp - (int) betp == 0) {
         moneyp = moneyp-betp;
         SetConsoleTextAttribute(h,10);
+        cout << "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n";
         cout << "You bet ";
         SetConsoleTextAttribute(h,3);
         cout << betp << "$ ";
@@ -50,14 +38,35 @@ void BetP(int &moneyp){
         SetConsoleTextAttribute(h,4);
         cout << moneyp << "$";
         SetConsoleTextAttribute(h,10);
-        cout << ")\n";
+        cout << ")\n\n";
+        cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n";
+        SetConsoleTextAttribute(h,7);
         break;
+    }
+    else if(betp > moneyp){
+        SetConsoleTextAttribute(h,4);
+        cout << "\n--------------------------------------------------------------------------------\n";
+        cout << "$$$ Are you serious? You put money more than you have! Try again the poor!!! $$$\n";
+        cout << "--------------------------------------------------------------------------------\n\n";
+    }
+    else if(betp - (int) betp != 0) {
+        SetConsoleTextAttribute(h,4);
+        cout << "\n---------------------------------------------------------------------------------\n";
+        cout << "$$$ I don't want some kind of pocket change...Try again with integer numbers. $$$\n";
+        cout << "---------------------------------------------------------------------------------\n\n";
+    }
+    else{
+        SetConsoleTextAttribute(h,4);
+        cout << "\n-----------------------------------------------------------------------\n";
+        cout << "$$$ Invalid input Man! Give me your money! Try again and quickly!!! $$$\n";
+        cout << "-----------------------------------------------------------------------\n\n";
+        cin.clear(); /*use to solve alphabet wrong input*/
     }
     }
 }
 
 int main(){
-    int money = 100;
+    int money = 1000;
     BetP(money);
     return 0;
 }
